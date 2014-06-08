310bd4ba7584fd3ff50a9c301f87697c1e8417ea
Roberto Orgiu <roberto.orgiu@gmail.com>
2014-06-08 11:38:38 +0200

    Aggiunge un secondo metodo per ottenere lo stesso risultato

--- app/src/main/java/tiwiz/utils/DrawingUtils.java
+++ app/src/main/java/tiwiz/utils/DrawingUtils.java
@@ -3,6 +3,7 @@ package tiwiz.utils;
 import android.content.Context;
 import android.graphics.ColorFilter;
 import android.graphics.LightingColorFilter;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 
 public class DrawingUtils {
@@ -11,6 +12,14 @@ public class DrawingUtils {
         Drawable whiteDrawable = mContext.getResources().getDrawable(whiteResId);
         final ColorFilter filter = new LightingColorFilter(finalColor, 0);
         whiteDrawable.mutate().setColorFilter(filter);
+
+        return whiteDrawable;
+    }
+
+    //metodo alternativo ma ugualmente funzionante
+    public static final Drawable getColoredDrawable2(Context mContext, int whiteResId, int finalColor) {
+        Drawable whiteDrawable = mContext.getResources().getDrawable(whiteResId);
+        whiteDrawable.mutate().setColorFilter(finalColor, PorterDuff.Mode.MULTIPLY);
 
         return whiteDrawable;
     }
